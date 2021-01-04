739a888c631d5d5b94b82898ae3c1404b971a6b3
demetriodroide <demeuforia@gmail.com>
2021-01-04 15:28:30 +0100

    css1

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -30,30 +30,35 @@ function RegisterPage() {
   return (
     <div className="Register">
         {redirected && <Redirect to={redirected}/> }
-        <div className='Facebook-login-bar-1'>
-              <label htmlFor="" className="elige-perfil-1">perfil 1</label>
-              <input type="checkbox" name="checkAge" className="elige-perfil-1"/>
-        </div> 
         
             <form action="" id='form-reg-1' className='Frame-1'  onSubmit={handleSubmit(onSubmit)}>
-              <label htmlFor="">Usuario</label>
-              <input type="text" name="user" id="user" ref={register}/>
-              <label htmlFor="">Mail</label>
-              <input type="text" name="mail" id="mail" ref={register}/>
-              <label htmlFor="">Pwd</label>
-              <input type="password" name="pwd" id="pwd" ref={register}/>
-              <label htmlFor="">Repite</label>
-              <input type="password" name="pwdR" id="pwdR" ref={register}/>
+              <fieldset>
+                <legend></legend>  
+              <label htmlFor="" className="labelform">Usuario</label>
+              <input type="text" name="user" id="user" ref={register} className="inputform"/>
+              <label htmlFor="" className="labelform">Mail</label>
+              <input type="text" name="mail" id="mail" ref={register} className="inputform"/>
+              <label htmlFor="" className="labelform">Pwd</label>
+              <input type="password" name="pwd" id="pwd" ref={register} className="inputform"/>
+              <label htmlFor="" className="labelform">Repite</label>
+              <input type="password" name="pwdR" id="pwdR" ref={register} className="inputform"/>
               <button  className='Vezapp-Button-Login'>Enviar</button>
+              </fieldset>
             </form>
-            
-        <div className='Facebook-login-bar-2'>
-          <label htmlFor="" className='elige-perfil-2'>perfil 2</label>
-          <input type="checkbox" name="checkAge" className="elige-perfil-2"/>
-        </div>
       
     </div>
   );
 }
 
 export default RegisterPage;
+/*
+<div className='Facebook-login-bar-1'>
+              <label htmlFor="" className="elige-perfil-1">perfil 1</label>
+              <input type="checkbox" name="checkAge" className="elige-perfil-1"/>
+        </div> 
+        
+        <div className='Facebook-login-bar-2'>
+          <label htmlFor="" className='elige-perfil-2'>perfil 2</label>
+          <input type="checkbox" name="checkAge" className="elige-perfil-2"/>
+        </div>
+        */
