407c28d6c3c65894367b70a9e70e7fb01f37c23f
demetriodroide <demeuforia@gmail.com>
2021-01-10 20:57:04 +0100

    info1beer

--- src/pages/Infobeer/Infobeer.jsx
+++ src/pages/Infobeer/Infobeer.jsx
@@ -7,6 +7,31 @@ import Back from '../../img/Back.png';
 
 
 function InfoBeer() {
+/*
+    function rotate() {
+				
+        if (current == 1) {
+            byId("#block-1").removeClass().addClass("active");
+            $("#block-2").removeClass().addClass("non-active-top");
+            $("#block-3").removeClass().addClass("non-active-bottom");
+        } else if (current == 2) {
+            $("#block-1").removeClass().addClass("non-active-bottom");
+            $("#block-2").removeClass().addClass("active");
+            $("#block-3").removeClass().addClass("non-active-top");
+        } else {
+            $("#block-1").removeClass().addClass("non-active-top");
+            $("#block-2").removeClass().addClass("non-active-bottom");
+            $("#block-3").removeClass().addClass("active");
+        }
+    
+    }
+
+    $("#rotator div").click(function() {
+        current = this.id.substr(6);			
+        rotate();
+    });
+
+    */
 
   return (
     <div className="screen-size screen-back" >
@@ -16,12 +41,13 @@ function InfoBeer() {
                                 </div>
         </div>
 
-                <div className="card-beer">
-                    <div className="position1"></div>
-                <div className="position2"><h3 className="description"> {this.props.beer.descr_full} </h3></div>
-                    <div className="position3"></div>
+                <div  id="rotator" className="card-beer">
+                    <div id="block-1" className="position1 rotation1"></div>
+                    <div id="block-2" className="position2 rotation1">{/*<h3 className="description"> this.props.beer.descr_full </h3>*/}</div>
+                    <div id="block-3" className="position3 rotation1"></div> 
                 </div>
-        </div>
+    </div>
+    
   );
 }
 
