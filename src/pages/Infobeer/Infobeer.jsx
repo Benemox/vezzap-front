bbd5c5bc66fe82f83cab7771faeb1fca7b9e00b1
demetriodroide <demeuforia@gmail.com>
2021-01-13 14:02:29 +0100

    casi ficha verveza

--- src/pages/Infobeer/Infobeer.jsx
+++ src/pages/Infobeer/Infobeer.jsx
@@ -41,36 +41,52 @@ function InfoBeer() {
             </Link>
         </div>
 
-                <div className="card-beer">
-                    <div className="card left">
-                        <div></div> 
-                        <div></div>
-                        <div></div>   
-                    </div>
+        <div className="card-beer">
+                <div className="card left">
+                     <div className="left-idea">Ideal con...</div> 
+                     <div className="left-maridaje"></div>
+                    <div className="left-maridaje"></div>
+                    <div className="left-maridaje"></div>   
+                </div>
                     
-                    <div className="card center">
-                        <div className=""><img src={Beer.beerInfo.image} alt="" className="center-img" /></div>
-                        <div className="center-title">{Beer.beerInfo.name}</div>
-                        <div className="center-short">{Beer.beerInfo.descr_short}</div>
-                    </div>
+                        <div className="card center">
+                                <div className=""><img src={Beer.beerInfo.image} alt="" className="center-img" /></div>
+                                <div className="center-title">{Beer.beerInfo.name}</div>
+                                <div className="center-short">{Beer.beerInfo.descr_short}</div>
+                        </div>
                         
-                    <div className="card right"> 
+                <div className="card right"> 
                         <div className="right-title">Sobre ella</div>
                         <div className="right-full">{Beer.beerInfo.descr_full}</div>
-                        <div className="ninja-estilo">Estilo</div>
-                        <div className="right-estilo">{Beer.beerInfo.estilo}</div>
-                        <div className="ninja-origen">Origen</div>
-                        <div className="right-origen">{Beer.beerInfo.origen}</div>
-
-                        
-                    </div>
                 
+                            <div className="boxone">
+                                    <div className="right-shadow">Fermentacion</div>
+                                    <div className="right-text">{Beer.beerInfo.fermentacion}</div>
+                            </div>
+                            <div className="boxone">        
+                                    <div className="right-shadow">Malta</div>
+                                    <div className="right-text">{Beer.beerInfo.maltas}</div>
+                            </div>
+                    <div className="boxtwo">
+                        <div className="boxhalf">
+                                <div className="right-shadow-half-left">IBU</div>
+                                <div className="right-text-half-left">{Beer.beerInfo.ibu}</div>
+                        </div>
+                        <div className="boxhalf">
+                                <div className="right-alcohol-half-right">{Beer.beerInfo.porcentaje_alcohol}</div>
+                                <div className="right-shadow-half-right">Alc.</div>
+                        </div>
+                    </div>    
                 </div>
-        <div className="Vboth">
-                <div className="Vleft" onClick={funcionrotaratras}> <img src={Vecleft} alt="" /> </div>
-                <div className="Vright" onClick={funcionrotaradelante}> <img src={Vecright} alt="" /> </div>
+                
         </div>
-                <NavBar/>
+
+
+                    <div className="Vboth">
+                            <div className="Vleft" onClick={funcionrotaratras}> <img src={Vecleft} alt="" /> </div>
+                            <div className="Vright" onClick={funcionrotaradelante}> <img src={Vecright} alt="" /> </div>
+                    </div>
+            <NavBar/>
     </div>
     
   );
@@ -81,15 +97,10 @@ export default InfoBeer;
 //<NavLink to="/faq" activeStyle={{ fontWeight: "bold", color: "red" }}></NavLink> para las bolas blancas :)
 // {Beer.beerInfo.name}
 // {Beer.beerInfo.color}
-// {Beer.beerInfo.origen}
-// {Beer.beerInfo.lupulos}
-// {Beer.beerInfo.estilo}
+// {Beer.beerInfo.ibu}
+// {Beer.beerInfo.maltas}
+// {Beer.beerInfo.fermentacion}
 // {Beer.beerInfo.descr_short}
 // {Beer.beerInfo.descr_full}
 // {Beer.beerInfo.porcentaje_alcohol}
 // {Beer.beerInfo.image}
-
-{/* <div className="ninja-lupulo">Lupulos</div>
-                        <div className="right-lupulo">{Beer.beerInfo.lupulos}</div>
-                        <div className="right-alcohol">{Beer.beerInfo.porcentaje_alcohol}</div>
-                        <div className="ninja-alcohol">Alc.</div> */}
